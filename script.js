20282f2506b8f6896600ce20541b8c1cb912b9b1
Krystal <youreggwhites@gmail.com>
2022-03-28 16:36:49 -0700

    Added play round and user input for rock, paper, scissors

--- script.js
+++ script.js
@@ -18,3 +18,58 @@ function computerPlay() {
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === computerSelection) {
+        return `Tied! No score counted`;
+    } 
+    else if (playerSelection === `rock` && computerSelection === `paper`) {
+        ++computerScore;
+        return `You Lose! Paper beats Rock`;
+    }
+    else if (playerSelection === `rock` && computerSelection === `scissors`) {
+        ++playerScore;
+        return `You Win! Rock beats Scissors`;
+    }
+    else if (playerSelection === `paper` && computerSelection === `rock`) {
+        ++playerScore;
+        return `You Win! Paper beats Rock`;
+    }
+    else if (playerSelection === `paper` && computerSelection === `scissors`) {
+        ++computerScore;
+        return `You Lose! Scissors beats Paper`;
+    }
+    else if (playerSelection === `scissors` && computerSelection === `paper`) {
+        ++playerScore;
+        return `You Win! Scissors beats Paper`;
+    }
+    else if (playerSelection === `scissors` && computerSelection === `rock`) {
+        ++computerScore;
+        return `You Lose! Rock beats Scissors`
+    }
+    else {
+        return "Invalid input. Try again.";
+        
+    }
+
+
+}
+
+
+
+function game() {
+
+    for (let i = 1; i <= 5; i++){
+        let userInput = prompt("Rock, Paper, or Scissors?");
+        const playerSelection = userInput.toLowerCase();
+        const computerSelection = computerPlay();
+        const results = playRound(playerSelection, computerSelection);
+        console.log(results);
+        console.log(`Round ${i}: Player Score: ${playerScore} Computer Score: ${computerScore}`);
+    }
+
+}
+
+
+game();
+
