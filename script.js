9ee53e54779598c241036f8cbd9000c4b6903ffb
Krystal <youreggwhites@gmail.com>
2022-04-07 16:49:03 -0700

    Condensed the playround function to make the code look cleaner.

--- script.js
+++ script.js
@@ -23,49 +23,48 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return `Tied! No score counted`;
     } 
-    else if (playerSelection === `rock` && computerSelection === `paper`) {
-        ++computerScore;
-        return `You Lose! Paper beats Rock`;
+    else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            ++computerScore;
+            return 'You Lose! Paper beats rock!';
+        }
+        else {
+            ++playerScore;
+            return 'You Win! Rock beats Scissors!'
+        }
     }
-    else if (playerSelection === `rock` && computerSelection === `scissors`) {
-        ++playerScore;
-        return `You Win! Rock beats Scissors`;
+    else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            ++playerScore;
+            return 'You Win! Paper beats Rock!';
+        }
+        else {
+            ++computerScore;
+            return 'You Lose! Scissors beats Paper!';
+        }
     }
-    else if (playerSelection === `paper` && computerSelection === `rock`) {
-        ++playerScore;
-        return `You Win! Paper beats Rock`;
+    else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            ++computerScore;
+            return 'You Lose! Rock beats Scissors!';
+        }
+        else {
+            ++playerScore;
+            return 'You Win! Scissors beats Paper!';
+        }
     }
-    else if (playerSelection === `paper` && computerSelection === `scissors`) {
-        ++computerScore;
-        return `You Lose! Scissors beats Paper`;
-    }
-    else if (playerSelection === `scissors` && computerSelection === `paper`) {
-        ++playerScore;
-        return `You Win! Scissors beats Paper`;
-    }
-    else if (playerSelection === `scissors` && computerSelection === `rock`) {
-        ++computerScore;
-        return `You Lose! Rock beats Scissors`
-    }
-    else {
-        return "Invalid input. Try again.";
-        
-    }
-
-
+    
 }
-
-
 
 function game() {
 
-    for (let i = 1; i <= 5; i++){
-        let userInput = prompt("Rock, Paper, or Scissors?");
+    for (let i = 1; i <= 5; i++) {
+        // let userInput = prompt("Rock, Paper, or Scissors?");
         const playerSelection = userInput.toLowerCase();
         const computerSelection = computerPlay();
         const results = playRound(playerSelection, computerSelection);
         console.log(results);
-        console.log(`Round ${i}: Player Score: ${playerScore} Computer Score: ${computerScore}`);
+        console.log(`Round ${i}: Player Score: ${playerScore} Computer Score: ${computerScore} Computer Choice : ${computerSelection} Player Choice: ${playerSelection}`);
     }
 
 }
